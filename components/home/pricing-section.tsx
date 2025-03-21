9a9817bba65038fe5efd62ad37013104b6b96f41
Naefunn <nathan.d.foster1@gmail.com>
2025-03-21 15:58:16 +0000

    added pricing section

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,43 +1,109 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { ArrowRight, CheckIcon } from "lucide-react";
+
+type PriceType = {
+  name: string;
+  price: number;
+  items: string[];
+  id: string;
+  paymentLink: string;
+  priceId: string;
+};
+
 const plans = [
   {
     id: "free",
+    name: "Free",
+    price: 0,
+    items: ["5 PDFs per month", "1 Page"],
+    paymentLink: "",
+    priceId: "",
   },
   {
     id: "basic",
+    name: "Basic",
+    price: 5,
+    items: ["10 PDFs per month", "5 Pages"],
+    paymentLink: "",
+    priceId: "",
   },
   {
     id: "pro",
+    name: "Pro",
+    price: 10,
+    items: ["Unlimited PDFs", "No Page Limit", "Markdown Export"],
+    paymentLink: "",
+    priceId: "",
   },
 ];
 
-const PricingCard = () => {
-  return <div>Pricing Card</div>;
+const PricingCard = ({
+  name,
+  price,
+  items,
+  id,
+  paymentLink,
+  priceId,
+}: PriceType) => {
+  return (
+    <div className="relative w-full max-w-lg hover:scale-105 hover:transition-all duration-300">
+      <div
+        className={cn(
+          "relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 border-[1px] border-gray-500/20 rounded-2xl",
+          id === "pro" && "border-rose-500 gap-5 border-2"
+        )}
+      >
+        <div className="flex justify-between items-center gap-4">
+          <div>
+            <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
+          </div>
+        </div>
+
+        <div className="flex gap-2 ">
+          <p className="text-5xl tracking-tight font-extrabold">£{price}</p>
+          <div className="flex flex-col justify-end mb-[4px]">
+            <p className="text-xs uppercase font-semibold">GBP</p>
+            <p className="text-xs ">/month</p>
+          </div>
+        </div>
+        <div className="space-y-2.5 leading-relaxed text-base flex-1">
+          {items.map((item, idx) => (
+            <li key={idx} className="flex items-center gap-2">
+              <CheckIcon size={18} />
+              <span>{item}</span>
+            </li>
+          ))}
+        </div>
+        <div className="space-y-2 flex justify-center w-full">
+          <Link
+            href={paymentLink}
+            className={cn(
+              "w-full rounded-full flex items-center justify-center gap-2 bg-linear-to-r from-rose-800 to-rose-500 hover:from-rose-500 hover:to-rose-800 text-white border-2 py-2",
+              id === "pro"
+                ? "border-rose-900"
+                : "border-rose-100 from-rose-400 to-rose-500"
+            )}
+          >
+            Buy Now <ArrowRight size={18} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function PricingSection() {
   return (
-    <section>
+    <section className="relative overflow-hidden" id="pricing">
       <div
         className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-4
         lg:px-8 lg:pt-12"
       >
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0 -top-40
-            -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-30"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35% ",
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-1155/678
-          w-[36.125rem] -translate-x-0.5 rotate-[30deg]
-          bg-linear-to-br from-slate-700 via-stone-800 to-indigo-600 opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72rem]"
-          />
-        </div>
-        <div>
-          <h2>Pricing</h2>
+        <div className="flex items-center justify-center w-full pb-12">
+          <h2 className="uppercase font-bold text-xl mb-8 text-rose-500">
+            Pricing
+          </h2>
         </div>
         <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
           {plans.map((plan) => (
